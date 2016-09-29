f4b1f38722b3ac7e808e7b12c78f35898c9fca51
Manish Ojha <manishojhac3@gmail.com>
2016-09-29 18:59:03 +0545

    Initial Commit

---
+++ Ripper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ripper
+//
+//  Created by Manish Ojha on 9/29/16.
+//  Copyright © 2016 Manish Ojha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
